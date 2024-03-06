102f99e7a821bdd40870d5512e5671a5c225834f
Flanture <56704750+Flanture@users.noreply.github.com>
2024-03-06 21:58:18 +0800

    Add files via upload

--- Args.cpp
+++ Args.cpp
@@ -8,7 +8,7 @@ Args::~Args() {
     delete address;
 }
 
-void Args::usage(char *msg, int exit_status) {
+void Args::usage(char* msg, int exit_status) {
     fprintf(exit_status == 0 ? stdout : stderr, "%s", USAGE_TXT);
 
     if (msg) {
@@ -17,7 +17,7 @@ void Args::usage(char *msg, int exit_status) {
     exit(exit_status);
 }
 
-void Args::parse_args(int argc, char **argv) {
+void Args::parse_args(int argc, char** argv) {
     int c;
     opterr = 0;
     if (argc < 2) {
@@ -26,23 +26,23 @@ void Args::parse_args(int argc, char **argv) {
 
     while ((c = getopt(argc, argv, "g:t:k:")) != -1) {
         switch (c) {
-            case 'g': {
-                printf("%s\n", optarg);
-                address = strdup(optarg);
-                break;
-            }
+        case 'g': {
+            printf("%s\n", optarg);
+            address = strdup(optarg);
+            break;
+        }
 
-            case 't': {
-                sscanf(optarg, "%d", &NT);
-                break;
-            }
+        case 't': {
+            sscanf(optarg, "%d", &NT);
+            break;
+        }
 
-            case 'k': {
-                sscanf(optarg, "%d", &topk);
-            }
+        case 'k': {
+            sscanf(optarg, "%d", &topk);
+        }
 
-            default:
-                break;
+        default:
+            break;
         }
     }
     char dataset[5] = "";
